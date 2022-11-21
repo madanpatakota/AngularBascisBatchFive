3e131fd4080ff24a2f2cb449e047b7c873a5d9b6
MadanMohanReddy <madan.patakota@hotmail.com>
2022-11-21 09:04:37 +0530

    Direcitves

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,7 +1,5 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 // "@angular/core": "^14.2.0",
-
-
 
 // my plan is i wanted to write content , css , business logic
 // @Component({
@@ -9,90 +7,107 @@ import { Component } from "@angular/core";
 //      template : `<h1>hello world , this is the my firsta project.......</h1>
 //      <input type = text value='Angular123'>`,
 //      styles  :  ["h1{color:red}" , "input{ color : blue}"]
-     
+
 // })
 
-
 @Component({
-     selector:"app-customer",
-     templateUrl : "../customer/customer.component.html",
-     styleUrls   : ["../customer/customer.component.css"]
+  selector: 'app-customer',
+  templateUrl: '../customer/customer.component.html',
+  styleUrls: ['../customer/customer.component.css'],
 })
+export class CustomerCompnonent {
+  text = 'This is the my first project';
 
-export class CustomerCompnonent{
+  customerID = 10;
+
+  customerName = 'Clerk';
+
+  inputtype = 'radio';
+
+  getProjectStatus() {
+    return this.text;
+  }
+
+  status = true;
+
+  getFired() {
+    console.log('executed....');
+  }
+
+  customerLocation = 'New york';
+
+  showMessage(msg: HTMLInputElement) {
+    console.log(msg.value);
+    // console.log("Execuing");
+  }
+
+  employeeLocation = 'Delhi';
+
+  showEmpMessage(event: any) {
+    console.log(event);
+    // console.log("Execuing");
+  }
+
+  employeeRole = 'Admin';
+
+  applyRedColor = true;
+
+  applyBlueColor = true;
+
+  isStatus = false;
+
+  isStauts1 = false;
+
+  canApplyClass = false;
+
+  isDisplay = true;
+
+  numbers = [
+    'Zero',
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+  ];
+
+  customers = ['William', 'Clerk', 'Robert'];
+
+  onlineFoodShopping = {
+    Name: 'Biryani',
+    location: 'Hyderabad',
+  };
+
+  onlineFoodShoppingList = [
+    {
+      Name: 'Non-veg',
+      location: 'Hyderabad',
+    },
+    {
+      Name: 'veg',
+      location: 'Bangolore',
+    },
+    {
+      Name: 'Pizza',
+      location: 'Bangolore',
+    },
+    {
+     Name: 'Ice-cream',
+     location: 'Hyderabad',
+   }
+  ];
 
 
-     text = "This is the my first project";
 
-     customerID = 10;
-
-
-     customerName = "Clerk";
-
-     inputtype = "radio";
-
-
-     getProjectStatus(){
-          return this.text;
-     }
-
-     status = true;
-
-
-     getFired(){
-           console.log("executed....") ; 
-     }
+  switchName = "hasljasdbfasbdlj";
 
 
 
 
-       customerLocation = "New york";
-
-
-
-       showMessage(msg:HTMLInputElement){
-          console.log(msg.value);
-          // console.log("Execuing");
-       }
-
-
-
-
-
-       employeeLocation = "Delhi";
-
-
-
-       showEmpMessage(event:any){
-          console.log(event);
-          // console.log("Execuing");
-       }
-
-
-
-     
-
-       employeeRole = "Admin";
-
-
-
-       applyRedColor = true;
-
-       applyBlueColor = true;
-
-
-
-       isStatus = false;
-
-
-
-
-
-       isStauts1 = false;
-
-
-     
 
 
 }
-
